fd2a3e7b89f96fed96e60c8996af0be56a3c7b63
MrTex09 <bagente24@gmail.com>
2024-04-03 10:07:59 -0300

    ejercicio 4

--- Ejercicio_1_weird _algorithm.py
+++ Ejercicio_1_weird _algorithm.py
@@ -1,12 +1,12 @@
 def weird_algorithm(n):
-    sequence = [n]
+    lista = [n]
     while n != 1:
         if n % 2 != 0:
             n = n * 3 + 1
         else:
             n //= 2
-        sequence.append(n)
-    return sequence
+        lista.append(n)
+    return lista
 
 assert weird_algorithm(3) == [3, 10, 5, 16, 8, 4, 2, 1], "Error en el caso de prueba"
 
--- Ejercicio_4_alindrome_reorder.py
+++ Ejercicio_4_alindrome_reorder.py
@@ -1,1 +1,31 @@
-# def palindrome_reorder(S):
+def palindrome_reorder(S):
+    N = len(S)
+    resultado = [' '] * N
+
+    
+    frecuencia = {}
+    for caracter in S:
+        frecuencia[caracter] = frecuencia.get(caracter, 0) + 1
+
+    contador = 0
+    for value in frecuencia.values():
+        if value % 2 != 0:
+            contador += 1
+    if contador > 1:
+        return "NO SOLUTION"
+
+    izq, der = 0, N - 1
+    for caracter in frecuencia:
+        if frecuencia[caracter] % 2 == 1:
+            resultado[N // 2] = caracter
+            frecuencia[caracter] -= 1
+        while frecuencia[caracter] > 0:
+            resultado[izq] = resultado[der] = caracter
+            izq += 1
+            der -= 1
+            frecuencia[caracter] -= 2
+
+    return ''.join(resultado)
+S = "rreeccoon"
+print(palindrome_reorder(S))
+assert palindrome_reorder("aabbc") == "abcba", "Error en el caso de prueba"
